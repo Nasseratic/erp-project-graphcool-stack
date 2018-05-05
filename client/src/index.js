41e619f36d2b16a0b959b9e1f65ed4a50878968e
nasseratic <mhmeeaad@gmail.com>
2018-05-06 00:01:46 +0200

    ( Update ) Article

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import {
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home/Index";
-
+import Article from "./components/Article/articleContent";
 import Search from "./components/Search/OutputBox";
 import Page404 from "./components/Error/404";
 import Page500 from "./components/Error/500";
@@ -23,7 +23,7 @@ ReactDOM.render(
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/internal" component={null} />
-        <Route path="/article" component={null} />
+        <Route path="/article" component={Article} />
         <Route path="/search" component={Search} />
         <Route path="/404" component={Page404} />
         <Route path="/500" component={Page500} />
